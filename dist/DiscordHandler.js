ceaa7ddc73e8946ca638594dc8e900eb73427079
tizoy12 <62270450+tizoy12@users.noreply.github.com>
2021-07-12 07:58:22 +0800

    Update DiscordHandler.js

--- dist/DiscordHandler.js
+++ dist/DiscordHandler.js
@@ -73,9 +73,8 @@ exports.setupClient = setupClient;
 //# sourceMappingURL=DiscordHandler.js.map
 
 
-var http = require('http');
-
-http.createServer(function (req, res) {
-  res.write("I'm alive");
-  res.end();
-}).listen(8080);
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+});
